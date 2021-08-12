937396f7573b77fdfbac8129bf9a7c16097f12ab
HeyMehedi <mehedi.2800@gmail.com>
2021-08-12 21:28:00 +0600

    Placeholder Added

--- templates/menu-page/override-contents.php
+++ templates/menu-page/override-contents.php
@@ -19,7 +19,7 @@ use HeyMehedi\All_In_One_Content_Restriction\Helper;
 		<?php esc_html_e( 'Replace Default Excerpt', 'all-in-one-content-restriction' );?>
 		<span class="heymehedi_helper_text" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php esc_attr_e( 'Use the_content by %%excerpt%%', 'all-in-one-content-restriction' );?>"><?php esc_html_e( '?', 'all-in-one-content-restriction' );?></span>
 	</label>
-	<textarea class="form-control" name="heymehedi_the_excerpt" id="heymehedi_the_excerpt" rows="5"><?php echo wp_kses_post( $args['the_excerpt'] ); ?></textarea>
+	<textarea class="form-control" name="heymehedi_the_excerpt" id="heymehedi_the_excerpt" rows="5" placeholder="<?php esc_attr_e( 'Prefix %%excerpt%% Suffix', 'all-in-one-content-restriction' );?>"><?php echo wp_kses_post( $args['the_excerpt'] ); ?></textarea>
 
 	<label for="heymehedi_custom_editor" class="form-label">
 		<?php esc_html_e( 'Replace Default Description', 'all-in-one-content-restriction' );?>
