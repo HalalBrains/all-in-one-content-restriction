ce145501f9af3a7e8f99a1107554f628c63f0f52
HeyMehedi <mehedi.2800@gmail.com>
2021-08-14 21:28:45 +0600

    code santized

--- includes/model/Settings.php
+++ includes/model/Settings.php
@@ -28,19 +28,19 @@ class Settings {
 	}
 
 	public static function set( $data ) {
-		$settings                     = self::get();
-		$ids                          = $data['itemIds'];
-		$ids_by_in                    = $data['restrictionIn'] . '_ids';
-		$settings['post_type']        = sanitize_text_field( $data['posttype'] );
-		$settings['restrict_in']      = sanitize_text_field( $data['restrictionIn'] );
-		$settings['role_names']       = $data['roleNames'];
-		$settings['protection_type']  = sanitize_text_field( $data['protectionType'] );
-		$settings['redirection_type'] = sanitize_text_field( $data['redirectionType'] );
-		$settings['the_title']        = sanitize_text_field( $data['theTitle'] );
-		$settings['the_excerpt']      = sanitize_textarea_field( htmlentities( $data['theExcerpt'] ) );
-		$settings['the_content']      = sanitize_textarea_field( htmlentities( $data['theContent'] ) );
-		$settings['custom_url']       = esc_url( $data['customUrl'] );
-		$settings[$ids_by_in]         = $ids;
+		$settings                      = self::get();
+		$ids                           = self::sanitize_array( $data['itemIds'] );
+		$settings['post_type']         = sanitize_text_field( $data['posttype'] );
+		$settings['restrict_in']       = sanitize_text_field( $data['restrictionIn'] );
+		$ids_by_restrict_in            = $settings['post_type'] . "_" . $settings['restrict_in'] . '_ids';
+		$settings['role_names']        = self::sanitize_array( $data['roleNames'] );
+		$settings['protection_type']   = sanitize_text_field( $data['protectionType'] );
+		$settings['redirection_type']  = sanitize_text_field( $data['redirectionType'] );
+		$settings['the_title']         = sanitize_text_field( $data['theTitle'] );
+		$settings['the_excerpt']       = sanitize_textarea_field( htmlentities( $data['theExcerpt'] ) );
+		$settings['the_content']       = sanitize_textarea_field( htmlentities( $data['theContent'] ) );
+		$settings['custom_url']        = esc_url( $data['customUrl'] );
+		$settings[$ids_by_restrict_in] = $ids;
 
 		if ( user_can( wp_get_current_user(), 'manage_options' ) ) {
 			$is_submitted = update_option( 'all_in_one_content_restriction_settings', $settings, true );
@@ -52,5 +52,20 @@ class Settings {
 			}
 		}
 
+	}
+
+	private static function sanitize_array( $array ) {
+
+		foreach ( $array as &$value ) {
+			if ( ! is_array( $value ) ) {
+				// Sanitize if value is not an array
+				$value = sanitize_text_field( $value );
+			} else {
+				// Do inside this function again
+				self::sanitize_array( $value );
+			}
+		}
+
+		return $array;
 	}
 }
