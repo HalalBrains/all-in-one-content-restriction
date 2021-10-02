cc3db39edfdd2fc3233ea0e944d61a9bf1a4af58
HeyMehedi <mehedi.2800@gmail.com>
2021-10-02 21:43:48 +0600

    Sticky Details

--- templates/menu-page/selected-items.php
+++ templates/menu-page/selected-items.php
@@ -2,20 +2,20 @@
 /**
  * @author  HeyMehedi
  * @since   1.0
- * @version 1.1
+ * @version 1.1.1
  */
 
 use HeyMehedi\All_In_One_Content_Restriction\Helper;
 use HeyMehedi\All_In_One_Content_Restriction\Markup_Manager;
 
-$post_type                 = isset( $args['post_type'] ) ? $args['post_type'] : 'post';
-$restrict_in               = isset( $args['restrict_in'] ) ? $args['restrict_in'] : 'category';
+$post_type   = isset( $args['post_type'] ) ? $args['post_type'] : 'post';
+$restrict_in = isset( $args['restrict_in'] ) ? $args['restrict_in'] : 'category';
 ?>
-<div class="part5">
+<div class="part5 fixed-details">
 
 	<h2><?php esc_html_e( 'Rule Applied', 'all-in-one-content-restriction' );?></h2>
 	<p><?php esc_html_e( 'Restrict in ', 'all-in-one-content-restriction' );?><span id="restrict_in_title"><?php echo esc_html( Helper::get_current_restrict_in_label( $post_type, $restrict_in ) ); ?></span></p>
-	
+
 	<label for="heymehedi-search_bar_selected" class="form-label"><?php esc_html_e( 'Title or ID', 'all-in-one-content-restriction' );?></label>
 	<input id="heymehedi-search_bar_selected" type="text" class="form-control" placeholder="<?php esc_attr_e( 'Type to search', 'all-in-one-content-restriction' );?>">
 
@@ -35,14 +35,14 @@ $restrict_in               = isset( $args['restrict_in'] ) ? $args['restrict_in'
 
 			<tbody id="heymehedi-selected_items_table_body">
 
-				<?php 
+				<?php
 				echo Markup_Manager::display_taxonomy_single_items_html(
-				$post_type,
-				$restrict_in,
-				'dashicons-minus',
-				array(),
-				isset( $args['selected_ids'] ) ? $args['selected_ids'] : array(),
-				true );
+					$post_type,
+					$restrict_in,
+					'dashicons-minus',
+					array(),
+					isset( $args['selected_ids'] ) ? $args['selected_ids'] : array(),
+					true );
 				?>
 
 			</tbody>
