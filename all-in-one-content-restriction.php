f55a42151c841debe95f1666fb07a3be4234f9b7
HeyMehedi <mehedi.2800@gmail.com>
2021-11-27 22:26:11 +0600

    version update

--- all-in-one-content-restriction.php
+++ all-in-one-content-restriction.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/HeyMehedi/all-in-one-content-restriction
 Description: All in One Content Restriction - A simple and user-friendly plugin to restrict users / visitors from viewing posts by restricting access, as simple as that.
 Author: HeyMehedi
 Author URI: https://heymehedi.com
-version: 1.1.1
+version: 1.3
 License: GPLv2 or later
 Text Domain: all-in-one-content-restriction
 Domain Path: /languages
